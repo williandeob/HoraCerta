5171c96f213acd489ce9bc9341fc9bd4c60b4395
willian <williandeob@gmail.com>
2016-06-30 00:10:29 -0300

    tela de cadastro de medicamento

--- app/src/main/java/br/com/ufg/inf/horacerta/CadastroMedicamento.java
+++ app/src/main/java/br/com/ufg/inf/horacerta/CadastroMedicamento.java
@@ -2,15 +2,17 @@ package br.com.ufg.inf.horacerta;
 
 
 import android.app.DatePickerDialog;
-import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.ImageView;
@@ -21,7 +23,6 @@ import java.util.Calendar;
 
 public class CadastroMedicamento extends AppCompatActivity{
 
-    ImageView imagemMedicamento;
     TextView btnHrInicio;
     TextView btnDtInicio;
     FloatingActionButton fab;
@@ -30,9 +31,10 @@ public class CadastroMedicamento extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_medicamento);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        ImageView imagemMedicamento =(ImageView)findViewById(R.id.medicamentoImagem);
+
 
         fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -62,12 +64,36 @@ public class CadastroMedicamento extends AppCompatActivity{
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_cadastro_medicamento, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_salvar) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         // TODO Auto-generated method stub
         super.onActivityResult(requestCode, resultCode, data);
 
         if(requestCode == 0 && resultCode == RESULT_OK ) {
             Bitmap bp = (Bitmap) data.getExtras().get("data");
+            ImageView imagemMedicamento = (ImageView)findViewById(R.id.medicamentoImagem);
+            imagemMedicamento.getLayoutParams().height = (int) getResources().getDimension(R.dimen.imageview_height);
             imagemMedicamento.setImageBitmap(bp);
         }
     }
