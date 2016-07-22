e0e33fbb3638c5f3b3872299f6fd8678af42bc61
willian <williandeob@gmail.com>
2016-07-22 00:32:09 -0300

    alarme

--- app/src/main/java/br/com/ufg/inf/horacerta/AlarmeActivity.java
+++ app/src/main/java/br/com/ufg/inf/horacerta/AlarmeActivity.java
@@ -3,10 +3,13 @@ package br.com.ufg.inf.horacerta;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import dao.MedicamentoDAO;
@@ -25,8 +28,19 @@ public class AlarmeActivity extends AppCompatActivity {
         if(medicamento != null) {
             TextView nomeMedicamento = (TextView) findViewById(R.id.nomeMedicamentoAlarme);
             TextView descricaoDeUso = (TextView) findViewById(R.id.descricaoMedicamentoAlarme);
+            ImageView imagem = (ImageView) findViewById(R.id.medicamentoImagemAlarme);
+
             nomeMedicamento.setText(medicamento.getNome());
             descricaoDeUso.setText(medicamento.getDescricaoDoUso());
+
+            Bitmap bitmap;
+            if(medicamento.getImagem() == null){
+                bitmap = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.ic_photo);
+            }else {
+                bitmap = BitmapFactory.decodeByteArray(medicamento.getImagem(), 0, medicamento.getImagem().length);
+            }
+
+            imagem.setImageBitmap(bitmap);
         }
 
         FloatingActionButton fabPausarNotificacao = (FloatingActionButton) findViewById(R.id.fabPausarNotificacao);
